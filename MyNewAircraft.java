cfd44e12c894a6b8a36587d8d18f4ee72339ddaf
louisj1 <33876037+louisj1@users.noreply.github.com>
2018-04-21 03:33:11 -0400

    Add files via upload

---
+++ MyNewAircraft.java
@@ -0,0 +1,35 @@
+public class MyNewAircraft {
+    // Jacques Louis
+    public static void main(String[] args) {
+        System.out.println("Welcome to Fort Lauderdale Airport");
+
+        Aircraft aircraft = new Aircraft();
+
+
+
+
+        aircraft.setMake("Delta Airlines");
+        aircraft.setPilot("Jacques");
+        aircraft.setSpeed(350);
+        aircraft.setFuelCount(36000);
+
+
+
+        System.out.println("Thank you for choosing " + aircraft.getMake());
+        System.out.println("Your pilot for today is " + aircraft.getPilot());
+        System.out.println("We are cruising at: " + aircraft.getSpeed());
+        System.out.println("Amount of gallons fuel we used over 10 hrs flight is: " + aircraft.getFuelCount());
+        Strings.doStringsDemo();
+        Job.doJobDemo();
+        Pilot.doPilotDemo();
+        Strings2.doStrings2Demo();
+        Array.doArrayDemo();
+        Plane.doPlaneDemo();
+        Psi.doPsiDemo();
+        Catch.doCatchDemo();
+
+
+
+
+    }
+}
